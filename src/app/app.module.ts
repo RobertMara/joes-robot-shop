3e29a7f1385de720a60c8f8a48e18bfe28afcf44
Robert Mara <robert.mara@hey.com>
2025-10-29 16:56:19 -0400

    Middle of module 9 before pulling api-server code out of m8-match branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { SiteHeaderComponent } from './site-header/site-header.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
@@ -11,6 +12,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
   declarations: [
     AppComponent,
     HomeComponent,
+    CartComponent,
     CatalogComponent,
     SiteHeaderComponent,
     ProductDetailsComponent
