73688cffa810510433a05e7bfd65caf254bbaa4c
Robert Mara <robert.mara@hey.com>
2025-10-30 14:11:37 -0400

    Module 9 after "Defining and accessing route parameters" section

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { IProduct } from './product.model';
 import { CartService } from '../cart/cart.service';
 import { ProductService } from './product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'bot-catalog',
@@ -14,17 +15,24 @@ export class CatalogComponent {
 
   constructor(
     private cartSvc: CartService,
-    private productSvc: ProductService
+    private productSvc: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
     this.productSvc.getProducts().subscribe((products) => {
       this.products = products;
     });
+    this.route.params.subscribe((params) => {
+      this.filter = params['filter'] ?? '';
+    })
   }
 
   addToCart(product: IProduct) {
     this.cartSvc.add(product);
+    //Go to cart after adding a product:
+    this.router.navigate(['/cart']);
   }
 
   getFilteredProducts() {
