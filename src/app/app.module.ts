cbd08fe10c1f45950bd2ef7384b4a7edb6a6449c
Robert Mara <robert.mara@hey.com>
2025-10-31 12:50:24 -0400

    Module 10 intermediate check-in after section "providing better user feedback from our sign-in events"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -8,7 +10,7 @@ import { CatalogComponent } from './catalog/catalog.component';
 import { SiteHeaderComponent } from './site-header/site-header.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
-import { AppRoutingModule } from './app-routing.module';
+import { SignInComponent } from './user/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
@@ -17,9 +19,10 @@ import { AppRoutingModule } from './app-routing.module';
     CatalogComponent,
     SiteHeaderComponent,
     ProductDetailsComponent,
-    CartComponent
+    CartComponent,
+    SignInComponent
   ],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
